2504dad6d351304f7656f791f5da703ab4324e2f
Kate Sills <kate@agoric.com>
2020-10-16 22:21:10 -0700

    chore: check in defaults for CI

---
+++ ui/public/conf/defaults.js
@@ -0,0 +1,17 @@
+// GENERATED FROM /Users/katesills/code/dapp-card-store/api/deploy.js
+export default {
+  INSTANCE_BOARD_ID: '605437304',
+  INSTALLATION_BOARD_ID: '1202180815',
+  SELL_ITEMS_INSTALLATION_BOARD_ID: '813441138',
+  INVITE_BRAND_BOARD_ID: '1667979430',
+  brandBoardIds: {
+    Card: '1403739213',
+    Money: '815824725',
+  },
+  issuerBoardIds: {
+    Card: '433864835',
+    Money: '262188032',
+  },
+  BRIDGE_URL: 'http://127.0.0.1:8000',
+  API_URL: 'http://127.0.0.1:8000',
+};
