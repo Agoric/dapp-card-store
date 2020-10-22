8c8f8d107fcec706138e5ae14de7e6a84583b249
Kate Sills <kate@agoric.com>
2020-10-21 17:15:21 -0700

    chore: remove installationConstants from gitignore

---
+++ ui/public/conf/installationConstants.js
@@ -0,0 +1,6 @@
+// GENERATED FROM /Users/katesills/code/dapp-card-store/contract/deploy.js
+export default {
+  CONTRACT_NAME: 'cardStore',
+  INSTALLATION_BOARD_ID: '1202180815',
+  SELL_ITEMS_INSTALLATION_BOARD_ID: '813441138',
+};
