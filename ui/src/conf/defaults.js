d87295a0811803775bdbdc050047ecc9cd72e60b
Kate Sills <kate@agoric.com>
2021-04-05 19:54:18 -0700

    chore: update defaults

--- ui/src/conf/defaults.js
+++ ui/src/conf/defaults.js
@@ -1,16 +1,16 @@
 // GENERATED FROM /Users/katesills/code/dapp-card-store/api/deploy.js
 export default {
-  "INSTANCE_BOARD_ID": "865163032",
-  "INSTALLATION_BOARD_ID": "433864835",
-  "SELL_ITEMS_INSTALLATION_BOARD_ID": "796371127",
+  "INSTANCE_BOARD_ID": "794977354",
+  "INSTALLATION_BOARD_ID": "379944443",
+  "SELL_ITEMS_INSTALLATION_BOARD_ID": "1530295560",
   "INVITE_BRAND_BOARD_ID": "1456154132",
   "brandBoardIds": {
-    "Card": "1107051844",
-    "Money": "605437304"
+    "Card": "529406044",
+    "Money": "1576549616"
   },
   "issuerBoardIds": {
-    "Card": "177231517",
-    "Money": "1265655452"
+    "Card": "715805464",
+    "Money": "1667979430"
   },
   "BRIDGE_URL": "http://127.0.0.1:8000",
   "API_URL": "http://127.0.0.1:8000",
--- ui/src/conf/installationConstants.js
+++ ui/src/conf/installationConstants.js
@@ -1,6 +1,6 @@
 // GENERATED FROM /Users/katesills/code/dapp-card-store/contract/deploy.js
 export default {
   "CONTRACT_NAME": "cardStore",
-  "INSTALLATION_BOARD_ID": "433864835",
-  "SELL_ITEMS_INSTALLATION_BOARD_ID": "796371127"
+  "INSTALLATION_BOARD_ID": "379944443",
+  "SELL_ITEMS_INSTALLATION_BOARD_ID": "1530295560"
 };
