f9df17edc80bb7ff6d4dd1c8d2949e4c13068484
Michael FIG <mfig@agoric.com>
2021-04-06 21:46:40 -0600

    ci: use config for beta network to install UI

---
+++ ui/deploy/src/conf/defaults.js
@@ -0,0 +1,18 @@
+// GENERATED FROM /Users/michael/agoric/dapp-card-store/api/deploy.js
+export default {
+  "INSTANCE_BOARD_ID": "715805464",
+  "INSTALLATION_BOARD_ID": "794977354",
+  "SELL_ITEMS_INSTALLATION_BOARD_ID": "529406044",
+  "INVITE_BRAND_BOARD_ID": "1456154132",
+  "brandBoardIds": {
+    "Card": "474502210",
+    "Money": "1576549616"
+  },
+  "issuerBoardIds": {
+    "Card": "688500812",
+    "Money": "1667979430"
+  },
+  "BRIDGE_URL": "http://127.0.0.1:8000",
+  "API_URL": "http://127.0.0.1:8000",
+  "CONTRACT_NAME": "cardStore"
+};
