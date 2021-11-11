7bf1a6e29953b168e3b69eac931552c667c53b10
simpletron <thongitbk@gmail.com>
2021-11-11 09:31:37 +0700

    chore(ui): change the proposal template avoid confusion in variable name

--- ui/src/auction.js
+++ ui/src/auction.js
@@ -16,25 +16,22 @@ const makeBidOfferForCard = async ({
   assert(card, X`At least one card must be chosen to purchase`);
   const invitation = await E(publicFacet).makeBidInvitationForKey(card);
 
-  const cardAmount = {
-    pursePetname: cardPurse.pursePetname,
-    value: harden([card]),
-  };
-  const moneyAmount = {
-    pursePetname: tokenPurse.pursePetname,
-    value: price,
-  };
-
   const offerConfig = {
     // JSONable ID for this offer.  This is scoped to the origin.
     id: Date.now(),
     invitation,
     proposalTemplate: {
       want: {
-        Asset: cardAmount,
+        Asset: {
+          pursePetname: cardPurse.pursePetname,
+          value: harden([card]),
+        },
       },
       give: {
-        Bid: moneyAmount,
+        Bid: {
+          pursePetname: tokenPurse.pursePetname,
+          value: price,
+        },
       },
     },
   };
