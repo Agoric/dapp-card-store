3003fa7696d7c380673843a1ec6e3cbbbbd898c4
JorgeLopes-BytePitch <jorge.lopes@bytepitch.com>
2022-12-29 17:55:01 +0000

    execution updates

--- ui/src/conf/defaults.js
+++ ui/src/conf/defaults.js
@@ -1,19 +1,19 @@
-// GENERATED FROM /Users/michael/agoric/dapp-card-store/api/deploy.js
+// GENERATED FROM /Users/jorgelopes/Documents/GitHub/Agoric/Chainboard/assignments/agoric-assignment-6/api/deploy.js
 export default {
-  "INSTANCE_BOARD_ID": "1221243446",
-  "INSTALLATION_BOARD_ID": "1107051844",
-  "SELL_ITEMS_INSTALLATION_BOARD_ID": "177231517",
-  "INVITE_BRAND_BOARD_ID": "500716545",
+  "INSTANCE_BOARD_ID": "board05432",
+  "INSTALLATION_BOARD_ID": "board01029",
+  "AUCTION_ITEMS_INSTALLATION_BOARD_ID": "board00530",
+  "INVITE_BRAND_BOARD_ID": "board0371",
+  "BRIDGE_URL": "agoric-lookup:https://local.agoric.com?append=/bridge",
   "brandBoardIds": {
-    "Card": "379944443",
-    "Money": "1532665031"
+    "Card": "board02733",
+    "Money": "board0425"
   },
   "issuerBoardIds": {
-    "Card": "1530295560",
-    "Money": "1576549616"
+    "Card": "board01034",
+    "Money": "board03523"
   },
-  "pricePerCard": 1000000,
-  "BRIDGE_URL": "http://127.0.0.1:8000",
+  "minBidPerCard": 1000000,
   "API_URL": "http://127.0.0.1:8000",
   "CONTRACT_NAME": "cardStore"
 };
--- ui/src/conf/installationConstants.js
+++ ui/src/conf/installationConstants.js
@@ -1,6 +1,7 @@
-// GENERATED FROM /Users/michael/agoric/dapp-card-store/contract/deploy.js
+// GENERATED FROM /Users/jorgelopes/Documents/GitHub/Agoric/Chainboard/assignments/agoric-assignment-6/contract/deploy.js
 export default {
   "CONTRACT_NAME": "cardStore",
-  "INSTALLATION_BOARD_ID": "1107051844",
-  "SELL_ITEMS_INSTALLATION_BOARD_ID": "177231517"
+  "INSTALLATION_BOARD_ID": "board01029",
+  "AUCTION_INSTALLATION_BOARD_ID": "board04431",
+  "AUCTION_ITEMS_INSTALLATION_BOARD_ID": "board00530"
 };
