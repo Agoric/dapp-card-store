feb398979ff40c15340ddc6fa6edb021d15e52c2
Mathieu Hofman <mathieu@agoric.com>
2023-09-25 00:20:57 +0000

    switch to NPM getting-started-dev packages

--- ui/src/conf/installationConstants.js
+++ ui/src/conf/installationConstants.js
@@ -1,7 +1,7 @@
-// GENERATED FROM /Users/anil/WebstormProjects/agoric-samples/dapp-card-store/contract/deploy.js
+// GENERATED FROM /home/node/workspace/agoric/dapp-card-store/contract/deploy.js
 export default {
   "CONTRACT_NAME": "cardStore",
-  "INSTALLATION_BOARD_ID": "board01029",
-  "AUCTION_INSTALLATION_BOARD_ID": "board04431",
-  "AUCTION_ITEMS_INSTALLATION_BOARD_ID": "board00530"
+  "INSTALLATION_BOARD_ID": "board01422",
+  "AUCTION_INSTALLATION_BOARD_ID": "board05024",
+  "AUCTION_ITEMS_INSTALLATION_BOARD_ID": "board03523"
 };
